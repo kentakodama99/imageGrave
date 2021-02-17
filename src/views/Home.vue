96aaa6dbdf2f4bc6710bf4622bdd1d2a874f323d
kentakodama99 <kenta.kodama22jyt22@gmail.com>
2021-02-17 23:06:03 +0900

    frontのみ

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <input type="file" accept="image/jpeg, image/png" v-if="filebox" @change="onImageChange"/>
+      <button @click="uploadImage()">アップロード</button>
+    </div>
+    <div v-show="uploadedImage">
+      <div>
+        <img :src="uploadedImage"  class="imgstyle" alt="画像がありません">
+      </div>
+      <button @click="removeImage()">戻る</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      uploadedImage:"",
+      filebox:true
+    };
+  },
+  methods:{
+    // 画像の反映
+    onImageChange(e){
+      console.log(e)
+      const files = e.target.files || e.dataTransfer.files; //FileListに転送
+      this.createImage(files[0])
+    },
+    createImage(file){
+      const reader = new FileReader(); // FileReaderを作成
+      reader.onload = e => { //コンテンツを読み込み、利用可能になると発火
+        this.uploadedImage = e.target.result;  // 画像ファイルを base64 文字列に変換
+      };
+      reader.readAsDataURL(file); // 読み込みを実行
+    },
+    removeImage(){
+      this.uploadedImage=false;
+      this.filebox=false;
+      this.$nextTick(() => {this.filebox=true;})
+    },
+    uploadImage(){
+      // let formData = new FormData();
+      if(this.uploadedImage){
+        alert("画像のアップロードが完了しました。")
+      }else{
+        alert("画像を選択してください。")
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+.imgstyle{
+  width:50%;
+  height:auto;
+}
+</style>
